e308d5b5f2653a2ddca9d17765071d1c818baa93
MichaelSilva123 <128339859+MichaelSilva123@users.noreply.github.com>
2023-05-07 22:23:40 -0700

    Add files via upload

---
+++ Github Rectangle.py
@@ -0,0 +1,9 @@
+def calculate_area(length, width):
+    #This function calculates the area of a rectangle given its length and width.
+    area = length * width
+    return area
+# Calculate the area of a rectangle with length 5 and width 10
+area = calculate_area(8, 14)
+
+# Print the result
+print("The area of the rectangle is:", area)
